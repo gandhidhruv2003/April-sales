e5bc2025b5d7175024ae1fd86128234898820de2
gandhidhruv2003 <63443072+gandhidhruv2003@users.noreply.github.com>
2022-10-21 05:44:06 +0530

    Add files via upload

--- Sales April 2019.py
+++ Sales April 2019.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import combinations
+from collections import Counter
 df = pd.read_csv("C:\\Users\\Dhruv Gandhi\\Desktop\\Python\\Sales_Data\\Sales_April_2019.csv")
 df = df.replace({
             "Order ID" : "Order ID",
@@ -18,11 +20,28 @@ df["Date"] = df["Order Date"].str[3:5]
 df["City"] = df["Purchase Address"].apply(lambda x: x.split(",")[1]) + "," + df["Purchase Address"].apply(lambda x: x.split(",")[2][:3])
 df["Order Date"] = pd.to_datetime(df["Order Date"])
 df["Hour"] = df["Order Date"].dt.hour
+all_data = df[df["Order ID"].duplicated(keep=False)]
+all_data["Grouped"] = df.groupby("Order ID")["Product"].transform(lambda x: ",".join(x))
+product_group = df.groupby("Product")
+quantity_ordered = product_group.sum()["Quantity Ordered"]
 
 
 #Calculate maximum sale in the month
 max_sales = df["Sales"].max()
-print("Max sales: ",max_sales)      #Max sale in the month
+print("Max sales: ",max_sales)      #Maximum sale in the month
+
+#Calculate the most oftenly sold together
+all_data = all_data[["Order ID","Grouped"]].drop_duplicates()
+count = Counter()
+for row in all_data["Grouped"]:
+    row_list = row.split(",")
+    count.update((Counter(combinations(row_list,2))))
+
+print(count.most_common(10)[0])        #Most oftenly sold together
+
+#Price of each item
+price = df.groupby("Product").mean()["Price Each"]
+print(price)
 
 #Plot graph between date and sales
 date_range = range(1,31)
@@ -49,12 +68,20 @@ plt.show()
 
 #Plot graph between Hour and Sales
 hour_range = [hour for hour, all_data in df.groupby('Hour')]
-#results3 = df.groupby('Hour').sum()
 plt.plot(hour_range, df.groupby(["Hour"]).count())
 plt.xticks(hour_range)
 plt.title("Graph between Hour and Sales")
 plt.xlabel("Time(in hour)")
 plt.ylabel("Sales in April(in $)")
 plt.grid()
+
+plt.show()
+
+#Plot graph between Product and Product Quantity
+products = [product for product, all_data in product_group]
+plt.bar(products,quantity_ordered)
+plt.xticks(products,rotation="vertical",size=8)
+plt.xlabel("Product")
+plt.ylabel("Product quantity")
 
 plt.show()
